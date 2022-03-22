68f1a560d9598ce43938b767fb194a1f4adb9a2b
irazramin <irazramin@gmail.com>
2022-03-22 22:24:36 +0600

    cart added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,7 +3,14 @@ import Product from '../product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products,setProducts] = useState([]);
+    const [cart,setCarts] = useState([]);
 
+    const allPrice = cart.map(c => c.price);
+      const calculateTotalPrice = (previousValue,currentValue) => previousValue + currentValue;
+      
+
+    const totalPrice = allPrice.reduce(calculateTotalPrice, 0);
+   
     useEffect(() =>{
         fetch("products.json")
         .then(res => res.json())
@@ -11,16 +18,28 @@ const Shop = () => {
     },[])
 
     const handleAddToCart = (product) =>{
-      console.log(product.id)
+        const newItems = [...cart,product];
+        setCarts(newItems)
     }
   return (
-    <div className='shop-container'>
-      <div className='px-7  py-20 grid xl:grid-cols-4  lg:grid-cols-3 md:grid-cols-2  grid-cols-1 gap-11 '>
+    <div className='shop-container relative'>
+      <div className='px-10  py-20 grid xl:grid-cols-4  lg:grid-cols-3 md:grid-cols-2  grid-cols-1 gap-11 '>
         {products.map((product) => (
-          <Product key={product.id} product={product} handleAddToCart={handleAddToCart}/>
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
         ))}
       </div>
-      <div className=''>cart</div>
+      <div className='bg-orange-200   w-full right-0 p-6'>
+        <h4 className='text-2xl font-normal text-center mt-7 '>
+          Order Summery
+        </h4>
+
+        <p className='mt-12'>Selected Items : {cart.length}</p>
+        <p className='mt-6'>Total Price : {totalPrice} </p>
+      </div>
     </div>
   );
 }
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Product.css';
+  import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
 const Product = (props) => {
     const {handleAddToCart,product} = props;
   const { id,img, name, price, seller,ratings } = product;
@@ -11,18 +14,19 @@ const Product = (props) => {
         alt=''
       />
       <div className='mt-3 mx-3 '>
-        <h4 title={name} className='text-lg font-normal '>
-          {name.length > 20 ? name.slice(0, 15) + '......' : name}
+        <h4 title={name} className='text-lg font-medium capitalize'>
+          {name.length > 20 ? name.slice(0, 20) + '......' : name}
         </h4>
-        <p className='text-lg font-normal mt-1'>Price : ${price}</p>
+        <p className='text-lg font-medium mt-1'>Price : ${price}</p>
 
         <p className='text-base mt-7 text-slate-600'>Manufacture : {seller}</p>
         <p className='text-base mt-3 text-slate-600'>Rating : {ratings}</p>
       </div>
-      <button className='w-full border-t-2 border-zinc-400 h-12 rounded-b-lg  m-0 bg-orange-300 absolute bottom-0 left-0 hover:bg-orange-500'
-      onClick={() => handleAddToCart(props.product)}
+      <button
+        className='w-full border-t-2 border-zinc-400 h-12 rounded-b-lg  m-0 bg-orange-300 absolute bottom-0 left-0 hover:bg-orange-500'
+        onClick={() => handleAddToCart(props.product)}
       >
-        Add to Cart
+        <p>Add to Cart</p>
       </button>
     </div>
   );
